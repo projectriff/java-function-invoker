7cd6611b149dd3de134775f1ca5c2713a48c26c0
Thomas Risberg <trisberg@pivotal.io>
2018-05-23 09:13:22 -0400

    Update samples for windowing and main class detection (#79)
    - update README instructions
    
    - remove windowing from wordcount sample
    
    - rely on main class detection in SCFn in uppercase sample

--- samples/wordcount/src/main/java/functions/Wordcount.java
+++ samples/wordcount/src/main/java/functions/Wordcount.java
@@ -1,6 +1,5 @@
 package functions;
 
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,8 +10,7 @@ public class Wordcount implements Function<Flux<String>, Flux<Map<String, Intege
 
 	@Override
 	public Flux<Map<String, Integer>> apply(Flux<String> words) {
-		return words.window(Duration.ofSeconds(10))
-				.flatMap(w -> w.collect(HashMap::new, (map, word) ->
-						map.merge(word, 1, Integer::sum)));
+		return Flux.from(words.collect(
+				HashMap::new, (map, word) -> map.merge(word, 1, Integer::sum)));
 	}
 }
